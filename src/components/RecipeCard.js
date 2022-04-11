4315e6a9a8bdb2dc5f9fe8e96a6dbd1d497c1e24
HooDini274 <102738818+HooDini274@users.noreply.github.com>
2022-04-11 16:45:04 +0200

    init

---
+++ src/components/RecipeCard.js
@@ -0,0 +1,20 @@
+import { Card, CardBody, CardTitle, CardSubtitle, CardImg } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+import placeholder from '../images/food-placeholder.png';
+
+export function RecipeCard({ title, preparationTime, slug }) {
+  return (
+    <Card className="h-100">
+      <Link to={`/recipe/${slug}`}>
+        <CardImg src={placeholder} alt="Preview" top />
+        <CardBody>
+          <CardTitle tag="h5">{title}</CardTitle>
+          <CardSubtitle>
+            {Math.floor(preparationTime / 60)} h {preparationTime % 60} min
+          </CardSubtitle>
+        </CardBody>
+      </Link>
+    </Card>
+  );
+}
